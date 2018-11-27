2c5b618ec397853e852adde75020353797545dfe
consihon <connorsihon@gmail.com>
2018-11-26 21:47:16 -0800

    all data appearing on screen

--- js/app.js
+++ js/app.js
@@ -1,92 +1,84 @@
 'use strict';
 
-function Picture(obj){
-    this.img_url = obj.image_url;
-    this.title = obj.title;
-    this.description = obj.description;
-    this.keyword = obj.keyword;
-    this.horns = obj.horns;
-    
-    if (!allKeys.includes(obj.keyword)) allKeys.push(obj.keyword);
-    allPics.push(this);
+function Picture(obj,page){
+  this.img_url = obj.image_url;
+  this.title = obj.title;
+  this.description = obj.description;
+  this.keyword = obj.keyword;
+  this.horns = obj.horns;
+  this.page = page;
+
+  if (!allKeys.includes(obj.keyword)) allKeys.push(obj.keyword);
+  allPics.push(this);
+  console.log(this);
 }
-$(function () {
-    // Grab the template script
-    var theTemplateScript = $("#expressions-template").html();
-  
-    // Compile the template
-    var theTemplate = Handlebars.compile(theTemplateScript);
-  
-    // Define our data object
-    var context={
-      "description": {
-        "escaped": "Using {{}} brackets will result in escaped HTML:",
-        "unescaped": "Using {{{}}} will leave the context as it is:"
-      },
-      "example": "<button> Hello </button>"
-    };
-  
-    // Pass our data to the template
-    var theCompiledHtml = theTemplate(context);
-  
-    // Add the compiled html to the page
-    $('.content-placeholder').html(theCompiledHtml);
-  });
-  
-Picture.prototype.render = function(key) {
-    if (!key||this.keyword===key){
-        $('main').append('<div class="clone"></div>');
-        let $clone = $('div[class="clone"]');
-        let picTemplate =$('#photo-template').html();
-        $clone.html(picTemplate);
-        $clone.find('h2').text(this.title);
-        $clone.find('img').attr('src', this.img_url);
-        $clone.find('p').text(this.description);
-        $clone.removeClass('clone');
-        $clone.attr('class', this.title);
-        $clone.attr('class', this.keyword);
-    }
-}
+
+
+Picture.prototype.render = function() {
+  $('body').append(`<div class="clone"></div>`);
+  let $clone = $('div[class="clone"]');
+  // Grab the template script
+  let pictureTemplate = $('#picture-template').html();
+
+  // Compile the template
+  var picTemplate = Handlebars.compile(pictureTemplate);
+
+  // Define our data object
+  let context={
+    'title':`${this.title}`,
+    'img':`<img src="${this.img_url}">` ,
+    'description': `${this.description}`
+  };
+  // Pass our data to the template
+  var theCompiledHtml = picTemplate(context);
+  // Add the compiled html to the page
+  $clone.html(theCompiledHtml);
+  $clone.attr('class', this.keyword);
+  $clone.attr('class', this.page);
+  $clone.removeClass('clone');
+
+};
 
 const allKeys = [];
 const allPics = [];
 
-function readJson () {
-    $.get('./data/page-1.json', 'json').then(data =>{
-        data.forEach(picObj=>{
-            new Picture(picObj);
-        })
-    }).then(() =>{
-        allKeys.forEach((key)=>{
-            $('select').append('<option class="opt"></option>');
-            let $opt = $('option[class="opt"]');
-            $opt.text(key);
-            $opt.removeClass('opt');
-            $opt.attr('id',key);
-        })
-    }).then(() => {
-        allPics.forEach(picture => {
-            picture.render();
-        })
+function readJson (json, page) {
+  $.get(json, 'json').then(data =>{
+    data.forEach(picObj=>{
+      new Picture(picObj,page);
     })
+  }).then(() =>{
+    allKeys.forEach((key)=>{
+      $('select').append('<option class="opt"></option>');
+      let $opt = $('option[class="opt"]');
+      $opt.text(key);
+      $opt.removeClass('opt');
+      $opt.attr('id',key);
+    })
+  }).then(() => {
+    allPics.forEach(picture => {
+      picture.render();
+    })
+  })
 }
 
-$("select").on("change", function(){
-    let selection = $(this).val();
-    console.log(selection);
-    if(selection==='default'){
-        $('main div').show();
-    }else{
-        $('main div').hide();
-        $('.'+selection).show();
-    }
+$('select').on('change', function(){
+  let selection = $(this).val();
+  console.log(selection);
+  if(selection==='default'){
+    $('main div').show();
+  }else{
+    $('main div').hide();
+    $('.'+selection).show();
+  }
 });
 
 
-readJson();
+readJson('./data/page-1.json',1);
+readJson('./data/page-2.json',2);
 
 
 
 function init (){
-    
+
 }
